ce357fc1888f201c581895468a9e51982c88e74c
nostalgebraist <nostalgebraist@gmail.com>
2020-12-27 18:28:01 -0800

    moving code from notebooks: refactor + add lenshead class

--- src/ecco/lenses.py
+++ src/ecco/lenses.py
@@ -7,36 +7,60 @@ import seaborn as sns
 from tqdm.autonotebook import tqdm, trange
 from sklearn.decomposition import sparse_encode
 
+import torch
 import ecco
+
+# TODO: move
+def _to_tensor(x, device):
+    return x.to(device) if isinstance(x, torch.Tensor) else torch.as_tensor(x).to(device)
+
+# TODO: move
+def _to_numpy(x):
+    return x.detach().cpu().numpy() if isinstance(x, torch.Tensor) else x
+
+class _LensHeadBase:
+    def __init__(self,
+                 fn,
+                 tensor_inputs_and_outputs=True,
+                 ):
+        self.fn = fn
+        self.tensor_inputs_and_outputs = tensor_inputs_and_outputs
+
+        if self.tensor_inputs_and_outputs:
+            self._to_input = _to_tensor
+        else:
+            self._to_input = lambda x, device: _to_numpy(x)
+
+    def __call__(self, h, h_prev=None, device='cpu'):
+        if h_prev is not None:
+            h_input = self._to_input(h, device) - self._to_input(h_prev, device)
+        else:
+            h_input = self._to_input(h, device)
+        out = self.fn(h_input)
+        out_tensor = _to_tensor(out, device)
+        return out_tensor
+
+
+class LensHead(_LensHeadBase):
+    def __init__(self, fn):
+        super().__init__(fn, tensor_inputs_and_outputs=True)
+
+
+class NumpyLensHead(_LensHeadBase):
+    def __init__(self, fn):
+        super().__init__(fn, tensor_inputs_and_outputs=False)
+
+
+NO_LENS = LensHead(fn=lambda x: x)
 
 
 def lensed_subblock_states(output: ecco.output.OutputSeq,
                            position=0,
-                           lens_head=None,
-                           lens_head_is_np=False,
+                           lens_head: _LensHeadBase=NO_LENS,
                            lens_head_on_diff=False,
                            subtract_means=True,
                            max_layers=None,
                            use_tqdm=False):
-    def _to_user(tensor, prev=None, prev_for_user=None):
-        if lens_head is not None:
-            if lens_head_is_np:
-              if lens_head_on_diff and prev is not None and prev_for_user is not None:
-                prev_ = prev.detach().cpu().numpy()
-                this_ = tensor.detach().cpu().numpy()
-                h_for_user = prev_for_user + lens_head(this_-prev_)
-              else:
-                h_for_user = lens_head(tensor.detach().cpu().numpy())
-            else:
-              if lens_head_on_diff and prev is not None and prev_for_user is not None:
-                diff = lens_head(output.to(tensor)-output.to(prev))
-                h_for_user = prev_for_user + diff.detach().cpu().numpy()
-              else:
-                h_for_user = lens_head(output.to(tensor)).detach().cpu().numpy()
-        else:
-            h_for_user = tensor.numpy()
-        return h_for_user
-
     subblock_state_array, names = output.subblock_states(
         position=position,
         subtract_means=subtract_means,
@@ -49,16 +73,22 @@ def lensed_subblock_states(output: ecco.output.OutputSeq,
     if use_tqdm:
         _iter = tqdm(_iter)
 
-    prev = None
-    prev_for_user = None
+    h_prev = None
+    prev_lensed = None
     for name, h in _iter:
-        h_for_user = _to_user(h, prev=prev, prev_for_user=prev_for_user)
-        rows.append(h_for_user)
+        head_out = lens_head(h, h_prev=h_prev, device=output.device)
+        if lens_head_on_diff and prev_lensed is not None:
+            h_lensed = prev_lensed + head_out
+        else:
+            h_lensed = head_out
 
-        prev = h
-        prev_for_user = h_for_user
+        rows.append(h_lensed)
 
-    a = np.stack(rows, axis=0)
+        h_prev = h
+        prev_lensed = h_lensed
+
+    rows_np = [_to_numpy(row) for row in rows]
+    a = np.stack(rows_np, axis=0)
 
     # TODO: cleanup
     pre_df = a[:, 0, :]  # remove position singleton axis
